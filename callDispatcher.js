6cd6b32fc1a6ce4ee00bb2d59d8c96f262e1fe0a
Julian Katz <jkatz@fluentstream.com>
2015-06-30 11:09:26 -0600

    Updated callDispatcher.js to work with reFactored liveserver

--- callDispatcher.js
+++ callDispatcher.js
@@ -8,10 +8,10 @@
  */
 function CallDispatcher(options){
 
-	this.initEvent = "getCalls";
-	this.namespace = "/phonecalls";
+    this.initEvent = "getCalls";
+    this.namespace = "/phonecalls";
 
-	LiveDispatcher.call(this, options);
+    LiveDispatcher.call(this, options);
 }
 
 CallDispatcher.prototype = new LiveDispatcher();
@@ -21,17 +21,25 @@ CallDispatcher.prototype = new LiveDispatcher();
 */
 CallDispatcher.prototype.listen = function(){
 
-	if(_.isFunction(this.ringHandler))
-		this.connection.on('ring', this.ringHandler);
-	if(_.isFunction(this.connectHandler))
-		this.connection.on('bridge', this.connectHandler);
-	if(_.isFunction(this.hangUpHandler))
-		this.connection.on('hangup', this.hangUpHandler);
-	if(_.isFunction(this.getCallsHandler))
-		this.connection.on("calls" , this.getCallsHandler);
+    if(_.isFunction(this.getCallsHandler))
+        this.connection.on("calls" , this.getCallsHandler);
+    if(_.isFunction(this.addLegHandler))
+        this.connection.on("addLeg", this.addLegHandler);
+    if(_.isFunction(this.removeLegHandler))
+        this.connection.on("removeLeg", this.removeLegHandler);
+    if(_.isFunction(this.callConnectHandler))
+        this.connection.on("callConnect", this.callConnectHandler);
+    if(_.isFunction(this.callDisconnectHandler))
+        this.connection.on("callDisconnect", this.callDisconnectHandler);
+    if(_.isFunction(this.bridgeDestroyHandler))
+        this.connection.on("bridgeDestroy", this.bridgeDestroyHandler);
+    if(_.isFunction(this.bridgeCreateHandler))
+        this.connection.on("bridgeCreate", this.bridgeCreateHandler);
+    if(_.isFunction(this.callTransferHandler))
+        this.connection.on("callTransfer", this.callTransferHandler);
 };
 
 CallDispatcher.prototype.getCalls = function(){
 
-	this.connection.emit("getCalls" , this.tenant);
+    this.connection.emit("getCalls" , this.tenant);
 }
