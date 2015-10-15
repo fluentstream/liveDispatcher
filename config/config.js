4d1464a32d8be73abd34f0b88234df17b7c4ffb4
Mathew <mnorby@fluentstream.com>
2015-10-15 12:01:00 -0600

    Added the SMS dispatcher to livedispatcher

--- config/config.js
+++ config/config.js
@@ -1,26 +1,26 @@
-// var config = {
-// 	"socket":{
-// 		"url":"https://my.fluentcloud.com",
-// 		"socket":443,
-// 		"secure":true,
-// 		"transport":"websocket"
-// 	},
-// 	"authentication":{
-// 		"url":"https://my.fluentcloud.com/api/v1/core/authenticate",
-// 		"httpType":"GET",
-// 		"maxAttempts":10
-// 	},
-// 	"ajax":{
-// 		"apiNamespace":{
-// 			"core":{
-// 				"url":"https://my.fluentcloud.com/api/v1/core/"
-// 			},
-// 			"call":{
-// 				"url":"https://my.fluentcloud.com/api/v1/call/"
-// 			}
-// 		}
-// 	}
-// };
+var config = {
+	"socket":{
+		"url":"https://my.fluentcloud.com",
+		"socket":443,
+		"secure":true,
+		"transport":"websocket"
+	},
+	"authentication":{
+		"url":"https://my.fluentcloud.com/api/v1/core/authenticate",
+		"httpType":"GET",
+		"maxAttempts":10
+	},
+	"ajax":{
+		"apiNamespace":{
+			"core":{
+				"url":"https://my.fluentcloud.com/api/v1/core/"
+			},
+			"call":{
+				"url":"https://my.fluentcloud.com/api/v1/call/"
+			}
+		}
+	}
+};
 
 // var config = {
 // 	"socket":{
@@ -76,28 +76,28 @@ var config = {
 */
 
 //BETA
-var config = {
-    "socket":{
-        "url":"https://beta.fluentcloud.com",
-        "socket":443,
-        "secure":true,
-        "transport":"websocket",
-        "connectAttempts":5,
-        "connectDelay":5000
-    },
-    "authentication":{
-        "url":"https://beta.fluentcloud.com/api/v1/core/authenticate",
-        "httpType":"GET",
-        "maxAttempts":10
-    },
-    "ajax":{
-        "apiNamespace":{
-            "core":{
-                "url":"https://beta.fluentcloud.com/api/v1/core/"
-            },
-            "call":{
-                "url":"https://beta.fluentcloud.com/api/v1/call/"
-            }
-        }
-    },
-};
+// var config = {
+//     "socket":{
+//         "url":"https://beta.fluentcloud.com",
+//         "socket":443,
+//         "secure":true,
+//         "transport":"websocket",
+//         "connectAttempts":5,
+//         "connectDelay":5000
+//     },
+//     "authentication":{
+//         "url":"https://beta.fluentcloud.com/api/v1/core/authenticate",
+//         "httpType":"GET",
+//         "maxAttempts":10
+//     },
+//     "ajax":{
+//         "apiNamespace":{
+//             "core":{
+//                 "url":"https://beta.fluentcloud.com/api/v1/core/"
+//             },
+//             "call":{
+//                 "url":"https://beta.fluentcloud.com/api/v1/call/"
+//             }
+//         }
+//     },
+// };
