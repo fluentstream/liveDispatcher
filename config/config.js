daf50871890a50c5f72eaa501b89560f2ef35ad2
Mathew <mnorby@fluentstream.com>
2015-10-15 16:44:58 -0600

    Finished up the SMS dispatcher

--- config/config.js
+++ config/config.js
@@ -1,26 +1,26 @@
-var config = {
-	"socket":{
-		"url":"https://my.fluentcloud.com",
-		"socket":443,
-		"secure":true,
-		"transport":"websocket"
-	},
-	"authentication":{
-		"url":"https://my.fluentcloud.com/api/v1/core/authenticate",
-		"httpType":"GET",
-		"maxAttempts":10
-	},
-	"ajax":{
-		"apiNamespace":{
-			"core":{
-				"url":"https://my.fluentcloud.com/api/v1/core/"
-			},
-			"call":{
-				"url":"https://my.fluentcloud.com/api/v1/call/"
-			}
-		}
-	}
-};
+// var config = {
+// 	"socket":{
+// 		"url":"https://my.fluentcloud.com",
+// 		"socket":443,
+// 		"secure":true,
+// 		"transport":"websocket"
+// 	},
+// 	"authentication":{
+// 		"url":"https://my.fluentcloud.com/api/v1/core/authenticate",
+// 		"httpType":"GET",
+// 		"maxAttempts":10
+// 	},
+// 	"ajax":{
+// 		"apiNamespace":{
+// 			"core":{
+// 				"url":"https://my.fluentcloud.com/api/v1/core/"
+// 			},
+// 			"call":{
+// 				"url":"https://my.fluentcloud.com/api/v1/call/"
+// 			}
+// 		}
+// 	}
+// };
 
 // var config = {
 // 	"socket":{
@@ -47,57 +47,55 @@ var config = {
 // 	}
 // };
 
-/*
-var config = {
-	"socket":{
-		"url":"http://localhost",
-		"socket":3000,
-		"secure":true,
-		"transport":"websocket",
-		"connectAttempts":5,
-		"connectDelay":5000
-	},
-	"authentication":{
-		"url":"https://beta.fluentcloud.com/api/v1/core/authenticate",
-		"httpType":"GET",
-		"maxAttempts":5
-	},
-	"ajax":{
-		"apiNamespace":{
-			"core":{
-				"url":"http://localhost/api/v1/core/"
-			},
-			"call":{
-				"url":"http://localhost/api/v1/call/"
-			}
-		}
-	}
-};
-*/
+// var config = {
+// 	"socket":{
+// 		"url":"http://localhost",
+// 		"socket":3000,
+// 		"secure":true,
+// 		"transport":"websocket",
+// 		"connectAttempts":5,
+// 		"connectDelay":5000
+// 	},
+// 	"authentication":{
+// 		"url":"https://beta.fluentcloud.com/api/v1/core/authenticate",
+// 		"httpType":"GET",
+// 		"maxAttempts":5
+// 	},
+// 	"ajax":{
+// 		"apiNamespace":{
+// 			"core":{
+// 				"url":"http://localhost/api/v1/core/"
+// 			},
+// 			"call":{
+// 				"url":"http://localhost/api/v1/call/"
+// 			}
+// 		}
+// 	}
+// };
 
 //BETA
-// var config = {
-//     "socket":{
-//         "url":"https://beta.fluentcloud.com",
-//         "socket":443,
-//         "secure":true,
-//         "transport":"websocket",
-//         "connectAttempts":5,
-//         "connectDelay":5000
-//     },
-//     "authentication":{
-//         "url":"https://beta.fluentcloud.com/api/v1/core/authenticate",
-//         "httpType":"GET",
-//         "maxAttempts":10
-//     },
-//     "ajax":{
-//         "apiNamespace":{
-//             "core":{
-//                 "url":"https://beta.fluentcloud.com/api/v1/core/"
-//             },
-//             "call":{
-//                 "url":"https://beta.fluentcloud.com/api/v1/call/"
-//             }
-//         }
-//     },
-// };
+var config = {
+    "socket":{
+        "url":"https://beta.fluentcloud.com",
+        "socket":443,
+        "secure":true,
+        "transport":"websocket",
+        "connectAttempts":5,
+        "connectDelay":5000
+    },
+    "authentication":{
+        "url":"https://beta.fluentcloud.com/api/v1/core/authenticate",
+        "httpType":"GET",
+        "maxAttempts":10
+    },
+    "ajax":{
+        "apiNamespace":{
+            "core":{
+                "url":"https://beta.fluentcloud.com/api/v1/core/"
+            },
+            "call":{
+                "url":"https://beta.fluentcloud.com/api/v1/call/"
+            }
+        }
+    },
+};
