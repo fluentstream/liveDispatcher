482146f6ee92177dd1b290447d82bf967cac82ca
Mathew Norby <mnorby@fluentstream.com>
2016-05-20 12:47:11 -0600

    Added a new dispatcher for ivrs

---
+++ autoAttendantDispatcher.js
@@ -0,0 +1,51 @@
+/**
+ * This is the derived class.
+ * Things that need to be defined:
+ *   * [Property] namespace
+ *   * [Function] live
+ * Optional Definitions:
+ *   * [Property] initEvent
+ */
+function AutoAttendantDispatcher(options){
+
+    // this.initEvent = "getCalls";
+    this.namespace = "/autoattendant";
+
+    LiveDispatcher.call(this, options);
+}
+
+AutoAttendantDispatcher.prototype = new LiveDispatcher();
+
+/**
+* This is the function that listens to the events and dispatches them out
+*/
+AutoAttendantDispatcher.prototype.listen = function(){
+
+    if(_.isFunction(this.keypressHandler))
+        this.connection.on("keypress" , this.keypressHandler);
+};
+
+// AutoAttendantDispatcher.prototype.run = function(){
+
+//     console.log("Running the child run...");
+//     this.authenticationMaxAttempts = this.config.authentication.maxAttempts;
+//     this.connect();
+
+//     var that = this;
+
+//     this.connection.on("connect" , function(event){
+//         console.log("We are connected to the node server....",event);
+
+//         setTimeout(function(){
+//             that.listen();
+//             that.subscribe();    
+//         },5000);
+        
+//     });
+//     // this.listen();
+// }
+
+// AutoAttendantDispatcher.prototype.getCalls = function(){
+
+//     this.connection.emit("getCalls" , this.tenant);
+// }
